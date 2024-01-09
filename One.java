eb7d1966c8bf69b368da4c547b4ac7d7fe718a02
vinutna <madhutechskills09@gamil.com>
2024-01-09 20:27:39 +0530

    making addition v1.1

--- One.java
+++ One.java
@@ -1,6 +1,11 @@
 class One
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
+        int a,b,c;
+        a=10;
+        b=20;
+        c=a+b;
         System.out.println("Hello World");
     }
 }
